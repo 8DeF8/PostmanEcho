a7209b7e8a4af3b6791b6830a4afe16353c03515
DeF <DeF-2008@yandex.ru>
2024-01-19 19:26:50 +0300

    Success

--- src/test/java/ru/netology/rest/PostmanEchoTest.java
+++ src/test/java/ru/netology/rest/PostmanEchoTest.java
@@ -12,7 +12,7 @@ class PostmanEchoTest {
         given()
                 .baseUri("https://postman-echo.com")
                 .contentType("text/plain; charset=UTF-8")
-                .body("No")
+                .body("Hi")
                 .when()
                 .post("/post")
                 .then()
